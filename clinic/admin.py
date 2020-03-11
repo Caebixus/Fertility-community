6f886ac7651c6b7a08a3dca82727bbfcf65b186e
Caebix <ma.davidlangr@gmail.com>
2020-03-11 14:49:25 +0100

    update currency

--- clinic/admin.py
+++ clinic/admin.py
@@ -4,7 +4,7 @@ from .models import BasicClinic
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'clinicName', 'clinicOwner', 'list_date', 'is_published', 'is_published_list_date', 'pro_is_published', 'pro_is_published_list_date')
+    list_display = ('id', 'clinicName', 'clinicOwner', 'list_date', 'is_published', 'is_published_list_date', 'pro_is_published', 'pro_is_published_list_date', 'type')
     list_filter = ('is_published', 'pro_is_published', 'verified_is_published', 'clinicOwner')
     search_fields = ('clinicName', 'clinicOwner__username')
 

7bb2c2cc5ab2e5914b3f39157e02972b435f1f6f
Caebix <ma.davidlangr@gmail.com>
2020-03-16 15:17:56 +0100

    bug

--- clinic/admin.py
+++ clinic/admin.py
@@ -4,6 +4,7 @@ from .models import BasicClinic
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
+    save_as = True
     list_display = ('id', 'clinicName', 'clinicOwner', 'list_date', 'is_published', 'is_published_list_date', 'pro_is_published', 'pro_is_published_list_date', 'type')
     list_filter = ('is_published', 'pro_is_published', 'verified_is_published', 'clinicOwner')
     search_fields = ('clinicName', 'clinicOwner__username')

b122ed5f4e2f7693cc365f5e299c7a7ac409a870
Caebix <ma.davidlangr@gmail.com>
2020-04-13 19:30:40 +0200

    search fields

--- clinic/admin.py
+++ clinic/admin.py
@@ -5,8 +5,8 @@ from .models import BasicClinic
 
 class PostAdmin(admin.ModelAdmin):
     save_as = True
-    list_display = ('id', 'clinicName', 'clinicOwner', 'list_date', 'is_published', 'is_published_list_date', 'pro_is_published', 'pro_is_published_list_date', 'type')
-    list_filter = ('is_published', 'pro_is_published', 'verified_is_published', 'clinicOwner')
-    search_fields = ('clinicName', 'clinicOwner__username')
+    list_display = ('id', 'clinicName', 'clinicOwner', 'list_date', 'is_published', 'is_published_list_date', 'pro_is_published', 'pro_is_published_list_date', 'type', 'clinicRegion', 'clinicState')
+    list_filter = ('is_published', 'pro_is_published', 'verified_is_published', 'clinicOwner', 'clinicRegion', 'clinicState', 'clinicCity')
+    search_fields = ('clinicName', 'clinicOwner__username', 'clinicRegion', 'clinicState', 'clinicCity')
 
 admin.site.register(BasicClinic, PostAdmin)

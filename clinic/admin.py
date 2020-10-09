0b18e22c0eb34199040867bf0e46ca7b927a4b9b
Caebix <ma.davidlangr@gmail.com>
2020-10-09 10:55:48 +0200

    update statelink

--- clinic/admin.py
+++ clinic/admin.py
@@ -29,7 +29,7 @@ class PostAdmin(admin.ModelAdmin):
     actions = ['clinicLocationLink_update_g']
 
     def clinicLocationLink_update_g(modeladmin, request, queryset):
-        queryset.update(clinicLocationLink='https://www.fertilitycommunity.com/ivf-cost')
+        queryset.update(clinicStateLink='https://www.fertilitycommunity.com/fertility-clinic-usa')
     clinicLocationLink_update_g.short_description = "Update from Admin"
 
 admin.site.register(BasicClinic, PostAdmin)

2abdc045cfa7d043f642887917bb3c05adebbc6d
Caebix <ma.davidlangr@gmail.com>
2020-12-22 15:43:59 +0100

    blog + author + packages logo

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Blog, Author
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    model = Blog
+    list_display = ('id', 'title', 'created_at', 'last_modified', 'blog_url',)
+
+class AuthorAdmin(admin.ModelAdmin):
+    model = Author
+    list_display = ('author', 'author_name', 'author_page_url',)
+
+
+admin.site.register(Blog, BlogAdmin)
+
+admin.site.register(Author, AuthorAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pic_blog = models.ImageField(upload_to='blogPhotos', blank=True, null=True)
+    TAG_CHOICES = (
+        ('IVF-Abroad', 'IVF-Abroad'),
+        )
+    tag = models.CharField(max_length=40, choices=TAG_CHOICES, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    blog_url = models.URLField()
+    minute_read = models.IntegerField(null=True, blank=True)
+
+class Author(models.Model):
+    author = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=40)
+    author_lastname = models.CharField(max_length=40)
+    author_pic = models.ImageField(upload_to='authorPhotos', blank=True, null=True)
+    author_small_bio = models.CharField(max_length=250)
+    author_description = models.TextField()
+    linkedin_url = models.URLField()
+    facebook_url = models.URLField()
+    twitter_url = models.URLField()
+    additional_url = models.URLField()
+    author_page_url = models.URLField()

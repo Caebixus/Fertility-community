fe83191c85c8a835ac5ed11bf07e4c602115afba
Caebix <ma.davidlangr@gmail.com>
2021-03-12 10:05:42 +0100

    https://www.fertilitycommunity.com/fertility-clinics/czech-republic

--- clinic/admin.py
+++ clinic/admin.py
@@ -31,7 +31,7 @@ class PostAdmin(admin.ModelAdmin):
     actions = ['clinicLocationLink_update_g']
 
     def clinicLocationLink_update_g(modeladmin, request, queryset):
-        queryset.update(clinicStateLink='https://www.fertilitycommunity.com/fertility-clinics/india')
+        queryset.update(clinicStateLink='https://www.fertilitycommunity.com/fertility-clinics/czech-republic')
     clinicLocationLink_update_g.short_description = "Update from Admin"
 
 class ExportClinicTreatments(resources.ModelResource):

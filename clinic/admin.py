9244a0287f48801bc2abbab308d7c095f29a9034
Caebix <ma.davidlangr@gmail.com>
2021-03-19 15:53:28 +0100

    update

--- clinic/admin.py
+++ clinic/admin.py
@@ -31,7 +31,7 @@ class PostAdmin(admin.ModelAdmin):
     actions = ['clinicLocationLink_update_g']
 
     def clinicLocationLink_update_g(modeladmin, request, queryset):
-        queryset.update(clinicCityLink='https://www.fertilitycommunity.com/fertility-clinics/uk/london')
+        queryset.update(clinicRegionLink='')
     clinicLocationLink_update_g.short_description = "Update from Admin"
 
 class ExportClinicTreatments(resources.ModelResource):

2e2497da5ea56ac2e59b63c46edb939047d75de2
Caebix <ma.davidlangr@gmail.com>
2021-06-08 20:48:36 +0200

    celery.py

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -5,6 +5,9 @@ from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
+
+BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+
 # celery settings for the demo_project
 app = Celery('Fertility')
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -19,3 +22,6 @@ app.conf.beat_schedule = {
 
 app.conf.timezone = 'UTC'
 app.autodiscover_tasks()
+
+app.conf.broker_url = BASE_REDIS_URL
+app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'

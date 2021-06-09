bdd8c82e3ce0b75fa50e83a8607c88e62bc160af
Caebix <ma.davidlangr@gmail.com>
2021-06-09 12:31:46 +0200

    supervisord

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -6,22 +6,12 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
 
-BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
-
 # celery settings for the demo_project
 app = Celery('Fertility')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 # here is the beat schedule dictionary defined
 
-app.conf.beat_schedule = {
-    'calculate-dti-every-10-sec': {
-        'task': 'base.tasks.calculate_dti',
-        'schedule': 5.0
-    },
-}
-
 app.conf.timezone = 'UTC'
 app.autodiscover_tasks()
 
-app.conf.broker_url = 'amqp://guest:guest@localhost:5672//'
 app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'

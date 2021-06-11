5d8062c975840ac067428fcb51435316bf9f3735
Caebix <ma.davidlangr@gmail.com>
2021-06-11 21:59:22 +0200

    vymazáno import django a django.setup()

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -1,17 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
 import sys
 
 from celery import Celery
 from celery.schedules import crontab
 
-import django
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/djangoadmin/pyapps/Fertility-community')))
-django.setup()
+
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
 
 # celery settings for the demo_project
 app = Celery('Fertility')

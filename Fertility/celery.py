a8470a2f26c88c9f0f096b0d516bf22af88a78da
Caebix <ma.davidlangr@gmail.com>
2021-06-12 10:49:24 +0200

    autodiscover 6

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -7,15 +7,14 @@ import sys
 from celery import Celery
 from celery.schedules import crontab
 
-
 from django.conf import settings
 from django.apps import apps
 
 
 # celery settings for the demo_project
 app = Celery('Fertility')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object(settings, namespace='CELERY')
 # here is the beat schedule dictionary defined
 
 
-app.autodiscover_tasks(['app.Fertility.entries'])
+app.autodiscover_tasks()

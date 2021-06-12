738600ce72881c2a9985bd233402efb951fbfd71
Caebix <ma.davidlangr@gmail.com>
2021-06-12 11:09:57 +0200

    whyyy

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -12,7 +12,7 @@ from django.apps import apps
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
 
 # celery settings for the demo_project
-app = Celery('Fertility', include=['base.tasks'])
+app = Celery('Fertility', include=['base.tasks', 'clinic.tasks'])
 app.config_from_object(settings, namespace='CELERY')
 # here is the beat schedule dictionary defined
 

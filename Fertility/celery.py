5e26b618af46a91514e1d8bb52d6c1bacf41c141
Caebix <ma.davidlangr@gmail.com>
2021-06-12 11:22:47 +0200

    lol

--- Fertility/celery.py
+++ Fertility/celery.py
@@ -7,13 +7,12 @@ from celery import Celery
 from celery.schedules import crontab
 
 from django.conf import settings
-from django.apps import apps
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fertility.settings')
 
 # celery settings for the demo_project
-app = Celery('Fertility', include=['base.tasks', 'clinic.tasks'])
-app.config_from_object(settings, namespace='CELERY')
+app = Celery('Fertility')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 # here is the beat schedule dictionary defined
 
 

3ece7418d37e0979764ec75904168f054c095b7c
Caebix <ma.davidlangr@gmail.com>
2021-11-24 07:43:21 +0100

    errors in packages - solved

--- clinic/validators.py
+++ clinic/validators.py
@@ -5,6 +5,6 @@ def validate_file_size(value):
     filesize= value.size
 
     if filesize > 524288:
-        raise ValidationError("The maximum file size that can be uploaded is 0,5MB")
+        raise ValidationError("The maximum image/file size that can be uploaded is 0,5MB, please decrease the size of this image.")
     else:
         return value

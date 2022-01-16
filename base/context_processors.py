a5588b9ccffed86ffea69fd6a8b0f6e25f33fb62
Caebix <ma.davidlangr@gmail.com>
2022-01-16 18:30:31 +0100

    IP address bug + snippets

--- base/context_processors.py
+++ base/context_processors.py
@@ -1,20 +1,23 @@
 from django.shortcuts import get_object_or_404
 
 from cookies.forms import AllCookiesAccepted
-from cookies.models import CookiesConsents
+from cookies.models import CookiesConsents, CookieSettings
 
 
 def cookies_check(request):
     session_key = request.session.session_key
     if CookiesConsents.objects.filter(session_id=session_key).exists():
         cookies = get_object_or_404(CookiesConsents, session_id=session_key)
+        cookies_setting = get_object_or_404(CookieSettings, pk=1)
         form = AllCookiesAccepted(instance=cookies)
         return {
             "cookies": cookies,
             "form": form,
+            "cookies_setting": cookies_setting,
         }
     else:
         form = AllCookiesAccepted(request.GET)
         return {
             "form": form,
+            "cookies_setting": cookies_setting,
         }
--- cookies/admin.py
+++ cookies/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import CookiesConsents
+from .models import CookiesConsents, CookieSettings
 
 
 class CookiesConsentsAdmin(admin.ModelAdmin):
     model = CookiesConsents
 
+class CookieSettingsAdmin(admin.ModelAdmin):
+    model = CookieSettings
+
 
 admin.site.register(CookiesConsents, CookiesConsentsAdmin)
+admin.site.register(CookieSettings, CookieSettingsAdmin)

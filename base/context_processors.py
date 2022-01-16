6965b4da20d8fb43c66a61961ab6176a9d1b8f42
Caebix <ma.davidlangr@gmail.com>
2022-01-16 18:32:53 +0100

    IP address bug + snippets

--- base/context_processors.py
+++ base/context_processors.py
@@ -8,13 +8,16 @@ def cookies_check(request):
     session_key = request.session.session_key
     if CookiesConsents.objects.filter(session_id=session_key).exists():
         cookies = get_object_or_404(CookiesConsents, session_id=session_key)
+        cookies_setting = get_object_or_404(CookieSettings, pk=1)
         form = AllCookiesAccepted(instance=cookies)
         return {
             "cookies": cookies,
             "form": form,
+            "cookies_setting": cookies_setting,
         }
     else:
         form = AllCookiesAccepted(request.GET)
         return {
             "form": form,
+            "cookies_setting": cookies_setting,
         }

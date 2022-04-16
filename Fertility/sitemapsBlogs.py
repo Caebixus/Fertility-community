41d30582c8ae760adc3870f24d2199ae2b809d4c
Caebix <ma.davidlangr@gmail.com>
2022-04-16 09:57:33 +0200

    portugal IVF

--- Fertility/sitemapsBlogs.py
+++ Fertility/sitemapsBlogs.py
@@ -24,6 +24,7 @@ class BlogsViewSitemap(sitemaps.Sitemap):
         'ivf_in_czech_republic',
         'ivf_in_slovakia',
         'ivf_in_prague',
+        'ivf_in_portugal',
         'ivfmeditation',
 
         'bestivfclinicsinprague',
